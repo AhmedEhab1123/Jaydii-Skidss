2e793b1e5912c32d449aecf62a2e49e0ffe22739
Glitch (lolibott) <none>
2020-05-21 10:02:30 +0000

    🍛💨 Checkpoint ./commands/slap.js:4791911/2

--- commands/slap.js
+++ commands/slap.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js')
 
-module.exports.run = (client, message, args, tools) => {
+  module.exports.run = (client, message, args, tools) => {
 
     var images = ["https://i.imgur.com/fm49srQ.gif", "https://i.imgur.com/o2SJYUS.gif", "https://i.imgur.com/Agwwaj6.gif", "https://i.gifer.com/cCX.gif", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7vgxCdD_ojfyfBGdZPeei11Hm7cK3zrPJTcsTYZ9Tl-dsA7upzw", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUPZp5heqxYIWj0l1G0KDC3o9YqJvmAG2FqP6n1BSVGwO5pnSMgg", "https://pa1.narvii.com/6067/68a2bb5829d993b3362a37dce55caa9c0949af6c_hq.gif", ];
     var rand = Math.floor(Math.random() * images.length);
