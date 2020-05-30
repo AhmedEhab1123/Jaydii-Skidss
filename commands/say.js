34210914da04ff797c56f07f02179a0ef2bfab3c
Glitch (zer0bot) <none>
2020-05-30 08:53:21 +0000

    👩‍❤️‍💋‍👩🎩 Checkpoint ./commands/say.js:4791911/948 ./watch.json:4791911/12

--- commands/say.js
+++ commands/say.js
@@ -1,11 +1,21 @@
 const Discord = module.require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+       
+  let everyone = args.join(" ");
+  if(message.content.startsWith('-say @everyone')){
+      if(!everyone) return message.reply(`i cant mention everyone`, )
+      message.delete();
+      disableMentions: 'everyone, here'
+      message.channel.send(`${saying}` , {disableMentions:'everyone'});
+      
+  }
+    
       let saying = args.join(" ");
       if(!saying) return message.reply(`please, give me a text`, )
       message.delete();
-      disableMentions: ''
-      message.channel.send(saying , {disableMentions:'everyone'});
+      disableMentions: 'everyone, here'
+      message.channel.send(`${saying}` , {disableMentions:'everyone'});
       
 
 }
