2c0d21560e843690b1c74d23e7ae32a42d401988
Glitch (iridescent-alert-shake) <none>
2020-07-30 13:56:37 +0000

    👡❤️ Checkpoint ./config.json:9899454/402 ./commands/about.js:6287197/227:9899454/107 ./notifications.json:9899454/202

--- commands/about.js
+++ commands/about.js
@@ -16,22 +16,23 @@ exports.run = (bot, message, args) => {
     
     const embedStats = new Discord.RichEmbed()
       .setAuthor(bot.user.username)
-      .setTitle("**Bot Stats** | Click to join support server")
-      .setFooter("Created by Jaydii with ❤. using discord.js")
-      .setURL(https://discord.gg/Q2At3nb)
+      .setTitle("**Bot Stats**")
+      .setFooter("Created by A FUCKIN SKID THAT CAN'T DO ANYSHIT")
+      //.setURL(https://discord.gg/Q2At3nb)
       .setColor("RANDOM")
       .addField("• Mem Usage", `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB`, true)
       .addField("• Uptime ", `${duration}`, true)
       .addField("• Users", `${bot.users.size.toLocaleString()}`, true)
       .addField("• Servers", `${bot.guilds.size.toLocaleString()}`, true)
       .addField("• Channels ", `${bot.channels.size.toLocaleString()}`, true)
-      .addField("• Discord.js", `v${version}`, true)
-      .addField("• Node", `${process.version}`, true)
+      .addField("• Discord.js", "V12.x.x", true)
+      .addField("• Node", "Node V. 12.x.x", true)
       .addField("• CPU", `\`\`\`md\n${os.cpus().map(i => `${i.model}`)[0]}\`\`\``)
       .addField("• CPU usage", `\`${percent.toFixed(2)}%\``, true)
       .addField("• Arch", `\`${os.arch()}\``, true)
       .addField("• Platform", `\`\`${os.platform()}\`\``, true)
-      .addField("API Latency", `${Math.round(bot.ping)}ms`);
+      .addField("API Latency", `${Math.round(bot.ping)}ms`)
+      .addField("[Support Server](https://google.com)\n[Add Bot Here](https://google.com)")
     
     message.channel.send(embedStats);
   });
